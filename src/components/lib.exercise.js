aac00d2ac5636d4797d6d04225960cad4519c8c9
Juan Ruiz <rmjuanruiz@gmail.com>
2023-10-06 17:34:30 -0500

    Work on React Add Styles chapter

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,5 +1,9 @@
-import styled from '@emotion/styled'
+import {keyframes} from '@emotion/core'
+import styled from '@emotion/styled/macro'
 import {Dialog as ReachDialog} from '@reach/dialog'
+import {FaSpinner} from 'react-icons/fa'
+import * as colors from 'styles/colors'
+import * as mq from 'styles/media-queries'
 
 // 🐨 create a button styled component here called "Button"
 // make it look nice and allow it to support a "variant" prop which can be
@@ -40,6 +44,38 @@ import {Dialog as ReachDialog} from '@reach/dialog'
 //   display: 'flex',
 //   flexDirection: 'column',
 
+const buttonVariants = {
+  primary: {
+    background: colors.indigo,
+    color: colors.base,
+  },
+  secondary: {
+    background: colors.gray,
+    color: colors.text,
+  },
+}
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  ({variant = 'primary'}) => buttonVariants[variant],
+)
+
+const Input = styled.input({
+  borderRadius: '3px',
+  border: `1px solid ${colors.gray10}`,
+  background: colors.gray,
+  padding: '8px 12px',
+})
+
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
 // 💰 I'm giving a few of these to you:
 const CircleButton = styled.button({
   borderRadius: '30px',
@@ -50,9 +86,9 @@ const CircleButton = styled.button({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'white',
-  color: '#434449',
-  border: `1px solid #f1f1f4`,
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
   cursor: 'pointer',
 })
 
@@ -62,10 +98,22 @@ const Dialog = styled(ReachDialog)({
   paddingBottom: '3.5em',
   boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
   margin: '20vh auto',
-  '@media (max-width: 991px)': {
+  [mq.small]: {
     width: '100%',
     margin: '10vh auto',
   },
 })
 
-export {CircleButton, Dialog}
+const spin = keyframes({
+  '0%': {transform: 'rotate(0deg)'},
+  '100%': {transform: 'rotate(360deg)'},
+})
+
+const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`,
+})
+Spinner.defaultProps = {
+  'aria-label': 'loading',
+}
+
+export {Button, Input, FormGroup, CircleButton, Dialog, Spinner}
